f2611cbdb79e4775d54c68b03594518603611121
Varda Jenkins <varda-jenkins@example.com>
2020-08-24 14:34:45 +0000

    Public branch commit

--- webapps/webapps/settings.py
+++ webapps/webapps/settings.py
@@ -113,15 +113,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'webapps.wsgi.application'
 
-
+# Access-Control-Allow-Origin
 # https://github.com/ottoyiu/django-cors-headers/
-
-# TODO: Remove this
-# https://jira.eduuni.fi/browse/CSCVARDA-392
-CORS_ORIGIN_ALLOW_ALL = True
-
-# CORS_ORIGIN_WHITELIST = ('localhost:4200', )
-# CORS_ORIGIN_REGEX_WHITELIST = (r'^(https://)?(\w+\.)?varda-db\.fi$', )
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = ['http://localhost:4200']
 
 
 # Database
