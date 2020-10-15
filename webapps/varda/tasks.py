36f32c3e9b05da387e8ff01e07c234ff2f5229af
Varda Jenkins <varda-jenkins@example.com>
2020-10-15 11:56:38 +0000

    Public branch commit

--- webapps/varda/tasks.py
+++ webapps/varda/tasks.py
@@ -1,3 +1,8 @@
+import logging
+import re
+from functools import wraps
+
+from django.core.cache import cache
 from celery import shared_task
 from django.contrib.contenttypes.models import ContentType
 from django.core.management import call_command
@@ -13,7 +18,39 @@ from varda.models import Henkilo, Taydennyskoulutus
 from varda.permission_groups import assign_object_permissions_to_taydennyskoulutus_groups
 
 
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
+
+def single_instance_task(timeout_in_minutes=8 * 60):
+    """
+    Decorator for celery task lock. Does not allow executing same task again if it's already running with matching id.
+    Note: Uses django memcached as non persistent storage which could be culled or crash losing all locks!
+    Note: Relies that task execution time is less than given timeout
+    Note: Args should be ids that contain no whitespaces since those will be parsed off
+    :param timeout_in_minutes: Time in minutes when lock will be released at latest. Should be greater than maximum task run time.
+    :return: Decorated function
+    """
+    def decorator(func):
+        @wraps(func)  # preserves func.__name__
+        def decorator_wrapper(*args, **kwargs):
+            arg_values = '{}{}'.format(args, kwargs)
+            cache_key_suffix = re.sub(r'\s+', '', arg_values)  # whitespaces are not allowed in cache keys
+            lock_id = 'celery-single-instance-{}-{}'.format(func.__name__, cache_key_suffix)
+            lock_timeout_in_seconds = timeout_in_minutes * 60
+            if cache.add(lock_id, 'true', lock_timeout_in_seconds):  # 'true' is never used value for the key
+                try:
+                    func(*args, **kwargs)
+                finally:
+                    cache.delete(lock_id)
+            else:
+                logger.error('Task already running with lock_id {}'.format(lock_id))
+        return decorator_wrapper
+    return decorator
+
+
 @shared_task
+@single_instance_task(timeout_in_minutes=1)
 def add(x, y):
     """
     This is currently only needed for testing, don't remove!
@@ -27,6 +64,7 @@ Use acks_late=True for the task-decoration, unless periodic task which always ge
 
 
 @shared_task
+@single_instance_task(timeout_in_minutes=8 * 60)
 def run_management_commands():
     """
     To be run periodically, e.g. to clean out expired sessions
@@ -36,37 +74,44 @@ def run_management_commands():
 
 
 @shared_task(acks_late=True)
+@single_instance_task(timeout_in_minutes=8 * 60)
 def remove_all_auth_tokens():
     from rest_framework.authtoken.models import Token
     Token.objects.all().delete()
 
 
 @shared_task
+@single_instance_task(timeout_in_minutes=8 * 60)
 def update_oph_staff_to_vakajarjestaja_groups():
     permission_groups.add_oph_staff_to_vakajarjestaja_katselija_groups()
 
 
 @shared_task
+@single_instance_task(timeout_in_minutes=8 * 60)
 def update_vakajarjestajat():
     organisaatiopalvelu.fetch_organisaatio_info()
 
 
 @shared_task
+@single_instance_task(timeout_in_minutes=8 * 60)
 def update_modified_henkilot():
     oppijanumerorekisteri.fetch_and_update_modified_henkilot()
 
 
 @shared_task
+@single_instance_task(timeout_in_minutes=8 * 60)
 def update_toimipaikat_in_organisaatiopalvelu_task():
     organisaatiopalvelu.update_toimipaikat_in_organisaatiopalvelu()
 
 
 @shared_task
+@single_instance_task(timeout_in_minutes=8 * 60)
 def fetch_huoltajat_task():
     oppijanumerorekisteri.fetch_huoltajat()
 
 
 @shared_task
+@single_instance_task(timeout_in_minutes=8 * 60)
 def update_all_organisaatio_service_originated_organisations_task():
     """
     Updates all organisations which data is managed by organisaatio service
@@ -76,11 +121,13 @@ def update_all_organisaatio_service_originated_organisations_task():
 
 
 @shared_task(acks_late=True)
+@single_instance_task(timeout_in_minutes=8 * 60)
 def update_henkilot_with_oid():
     oppijanumerorekisteri.fetch_henkilot_with_oid()
 
 
 @shared_task(acks_late=True)
+@single_instance_task(timeout_in_minutes=8 * 60)
 def update_henkilo_data_by_oid(henkilo_oid, henkilo_id, is_fetch_huoltajat=False):
     oppijanumerorekisteri.fetch_henkilo_data_by_oid(henkilo_oid, henkilo_id)
     if is_fetch_huoltajat:
@@ -88,6 +135,7 @@ def update_henkilo_data_by_oid(henkilo_oid, henkilo_id, is_fetch_huoltajat=False
 
 
 @shared_task
+@single_instance_task(timeout_in_minutes=8 * 60)
 def update_huoltajasuhteet_task():
     """
     Updates huoltajasuhde changes from oppijanumerorekisteri
@@ -97,11 +145,13 @@ def update_huoltajasuhteet_task():
 
 
 @shared_task
+@single_instance_task(timeout_in_minutes=8 * 60)
 def send_audit_log_to_aws_task():
     audit_log.collect_audit_log_and_send_to_aws()
 
 
 @shared_task
+@single_instance_task(timeout_in_minutes=8 * 60)
 def guardian_clean_orphan_objects():
     # from guardian.utils import clean_orphan_obj_perms
     # clean_orphan_obj_perms() - We do not want to run this currently.
@@ -109,6 +159,7 @@ def guardian_clean_orphan_objects():
 
 
 @shared_task(acks_late=True)
+@single_instance_task(timeout_in_minutes=8 * 60)
 def change_paos_tallentaja_organization_task(jarjestaja_kunta_organisaatio_id, tuottaja_organisaatio_id,
                                              tallentaja_organisaatio_id, voimassa_kytkin):
     permissions.change_paos_tallentaja_organization(jarjestaja_kunta_organisaatio_id, tuottaja_organisaatio_id,
@@ -116,6 +167,7 @@ def change_paos_tallentaja_organization_task(jarjestaja_kunta_organisaatio_id, t
 
 
 @shared_task(acks_late=True)
+@single_instance_task(timeout_in_minutes=8 * 60)
 def delete_object_permissions_explicitly_task(content_type_id, instance_id):
     """
     Object permissions need to be deleted explicitly:
@@ -128,11 +180,13 @@ def delete_object_permissions_explicitly_task(content_type_id, instance_id):
 
 
 @shared_task
+@single_instance_task(timeout_in_minutes=8 * 60)
 def update_koodistot_task():
     koodistopalvelu.update_koodistot()
 
 
 @shared_task
+@single_instance_task(timeout_in_minutes=8 * 60)
 def remove_address_information_from_tyontekijat_only_task():
     henkilot = Henkilo.objects.filter(~Q(kotikunta_koodi='') | ~Q(katuosoite='') |
                                       ~Q(postinumero='') | ~Q(postitoimipaikka=''),
@@ -145,6 +199,7 @@ def remove_address_information_from_tyontekijat_only_task():
 
 
 @shared_task(acks_late=True)
+@single_instance_task(timeout_in_minutes=8 * 60)
 def assign_taydennyskoulutus_permissions_for_toimipaikka_task(vakajarjestaja_oid, toimipaikka_oid):
     """
     Assign object level permissions to all taydennyskoulutukset of given vakajarjestaja for toimipaikka level groups
