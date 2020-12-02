83da58de0110189322c313178d2944d3264e079f
Varda Jenkins <varda-jenkins@example.com>
2020-12-02 09:37:51 +0000

    Public branch commit

--- webapps/varda/cas/oppija_cas_views.py
+++ webapps/varda/cas/oppija_cas_views.py
@@ -1,26 +1,59 @@
 import importlib.util
 import sys
 
+from rest_framework import status
+
 from varda.cas.cas_settings import settings
 
+
+def load_in_new_module(library_name, name):
+    spec = importlib.util.find_spec(library_name)
+    cas_oppija_specific_library = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(cas_oppija_specific_library)
+    sys.modules['varda_cas_oppija_' + name] = cas_oppija_specific_library
+    del spec
+    return cas_oppija_specific_library
+
+
 # Loading in separate module to avoid conflicting with django settings
-VIEWS_SPEC = importlib.util.find_spec('django_cas_ng.views')
-cas_oppija_views = importlib.util.module_from_spec(VIEWS_SPEC)
-VIEWS_SPEC.loader.exec_module(cas_oppija_views)
-sys.modules['varda_cas_oppija_views'] = cas_oppija_views
-del VIEWS_SPEC
+cas_oppija_views = load_in_new_module('django_cas_ng.views', 'views')
+cas_oppija_utils = load_in_new_module('django_cas_ng.utils', 'utils')
+cas_oppija_backends = load_in_new_module('django_cas_ng.backends', 'backends')
 
-UTILS_SPEC = importlib.util.find_spec('django_cas_ng.utils')
-cas_oppija_utils = importlib.util.module_from_spec(UTILS_SPEC)
-UTILS_SPEC.loader.exec_module(cas_oppija_utils)
-sys.modules['varda_cas_oppija_utils'] = cas_oppija_utils
-del UTILS_SPEC
+# Overriding these for caller-id decoration
+cas = load_in_new_module('cas', 'cas')
+requests_api = load_in_new_module('requests.api', 'requests_api')
 
-BACKENDS_SPEC = importlib.util.find_spec('django_cas_ng.backends')
-cas_oppija_backends = importlib.util.module_from_spec(BACKENDS_SPEC)
-BACKENDS_SPEC.loader.exec_module(cas_oppija_backends)
-sys.modules['varda_cas_oppija_backends'] = cas_oppija_backends
-del BACKENDS_SPEC
+
+def get_with_callerid(url, params=None, **kwargs):
+    kwargs.setdefault('allow_redirects', True)
+    old_headers = kwargs.get('headers', {})
+    headers = {**old_headers, **{'Caller-Id': 'csc.varda'}}
+    return requests_api.request('get', url, params=params, headers=headers, **kwargs)
+
+
+requests_api.get = get_with_callerid
+cas.requests = requests_api
+
+
+def get_login_forward_decorator(function):
+    """
+    This needs to be dynamically wrapped unless we want create whole new view with it's own dependencies that use
+    decorated version of get_cas_client function that either adds valtuudet query param true or false through
+    extra_login_params kwarg.
+    :param function: decorated function
+    :return: decorator function
+    """
+    def wrap_decorator(*args, **kwargs):
+        request = args[0]
+        query_params = request.GET
+        valtuudet = query_params.get('valtuudet', None)
+        response = function(*args, **kwargs)
+        if valtuudet is not None and response.status_code == status.HTTP_302_FOUND and not request.user.is_authenticated:
+            separator = '&' if '?' in response['Location'] else '?'
+            response['Location'] += f'{separator}valtuudet={"false" if valtuudet == "false" else "true"}'
+        return response
+    return wrap_decorator
 
 
 class OppijaCasLoginView(cas_oppija_views.LoginView):
@@ -28,12 +61,13 @@ class OppijaCasLoginView(cas_oppija_views.LoginView):
     django_cas_ng does not support multiple cas filters so we need to instantiate one by ourselves.
     """
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+        super(OppijaCasLoginView, self).__init__(**kwargs)
 
         # Monkey patching django settings super class module uses
         cas_oppija_views.settings = settings
         cas_oppija_utils.django_settings = settings
         cas_oppija_views.get_cas_client = cas_oppija_utils.get_cas_client
+        self.get = get_login_forward_decorator(self.get)
 
 
 # No need to override logout view settings since we can use the same one as virkailija-cas
@@ -50,6 +84,7 @@ class OppijaCASBackend(cas_oppija_backends.CASBackend):
 
         # Monkey patch utils import to use our version
         cas_oppija_utils.django_settings = settings
+        cas_oppija_utils.CASClient = cas.CASClient
         cas_oppija_backends.get_cas_client = cas_oppija_utils.get_cas_client
         cas_oppija_backends.settings = settings
 
