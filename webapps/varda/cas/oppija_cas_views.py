87eb0612b6fc50f9cac0a626067c2e51cc2b5ad4
Varda Jenkins <varda-jenkins@example.com>
2021-01-29 09:05:32 +0000

    Public branch commit

--- webapps/varda/cas/oppija_cas_views.py
+++ webapps/varda/cas/oppija_cas_views.py
@@ -50,8 +50,12 @@ def get_login_forward_decorator(function):
         valtuudet = query_params.get('valtuudet', None)
         response = function(*args, **kwargs)
         if valtuudet is not None and response.status_code == status.HTTP_302_FOUND and not request.user.is_authenticated:
-            separator = '&' if '?' in response['Location'] else '?'
-            response['Location'] += f'{separator}valtuudet={"false" if valtuudet == "false" else "true"}'
+            # Since CAS-oppija has trouble with storing valtuudet to inner state (OPHVARDA-2181) so we have to attach it
+            # to service parameter which is natively available. Since service is url encoded we need to use same format.
+            # E.g.: /cas-oppija/login?service=http%3A%2F%2Flocalhost%3A8000%2Faccounts%2Fhuoltaja-login%3Fnext%3D%252Fapi%252Foppija%252Fv1%252F
+            # Note: This is quick fix. Once CAS-oppija handles valtuudet parameter properly this should be reverted.
+            separator = '%26' if '%3F' in response['Location'] else '%3F'
+            response['Location'] += f'{separator}valtuudet%3D{"false" if valtuudet == "false" else "true"}'
         return response
     return wrap_decorator
 
@@ -90,5 +94,9 @@ class OppijaCASBackend(cas_oppija_backends.CASBackend):
 
     def authenticate(self, request, ticket, service):
         if request.resolver_match.view_name == 'oppija_cas_ng_login':
+            # By changing the service parameter above (OPHVARDA-2181) we have to manually add it or we can't validate
+            # service ticket (ST) because service differs from the one the ST was granted to.
+            if (valtuudet := request.GET.get('valtuudet')) is not None:
+                service += f'&valtuudet={"true" if valtuudet == "true" else "false"}'
             return super().authenticate(request, ticket, service)
         return None
