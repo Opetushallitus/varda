09e77770e94960ba35fd7378bccbcfc3cfd3db91
Varda Jenkins <varda-jenkins@example.com>
2021-02-23 07:42:13 +0000

    Public branch commit

--- client/projects/virkailija-app/src/app/varda-main/components/varda-reporting/varda-yhteenveto/varda-yhteenveto.component.ts
+++ client/projects/virkailija-app/src/app/varda-main/components/varda-reporting/varda-yhteenveto/varda-yhteenveto.component.ts
@@ -9,6 +9,7 @@ import { VardaKoosteApiService } from 'projects/virkailija-app/src/app/core/serv
 interface YhteenvetoElement {
   name: string;
   value: number;
+  tooltip: string;
   status: string;
 }
 
@@ -24,7 +25,7 @@ export class VardaYhteenvetoComponent implements OnInit {
   datasourceToimipaikat: Array<YhteenvetoElement> = [];
   datasourceTyontekijat: Array<YhteenvetoElement> = [];
   selectedVakajarjestaja: VardaVakajarjestajaUi;
-  displayedColumns = ['name', 'value'];
+  displayedColumns = ['name', 'value', 'tooltip'];
 
   constructor(
     private koosteService: VardaKoosteApiService,
@@ -40,38 +41,39 @@ export class VardaYhteenvetoComponent implements OnInit {
   fetchYhteenveto(): void {
     this.getYhteenvetoForReporting().subscribe(yhteenveto => {
       this.datasourceLapset = [
-        [this.i18n.yhteenveto_lapset_lkm, yhteenveto.lapset_lkm],
-        [this.i18n.yhteenveto_lapset_vakapaatos_voimassaoleva, yhteenveto.lapset_vakapaatos_voimassaoleva],
-        [this.i18n.yhteenveto_lapset_vakasuhde_voimassaoleva, yhteenveto.lapset_vakasuhde_voimassaoleva],
-        [this.i18n.yhteenveto_lapset_vuorohoidossa, yhteenveto.lapset_vuorohoidossa],
-        [this.i18n.yhteenveto_lapset_palveluseteli_ja_ostopalvelu, yhteenveto.lapset_palveluseteli_ja_ostopalvelu],
-        [this.i18n.yhteenveto_lapset_maksutieto_voimassaoleva, yhteenveto.lapset_maksutieto_voimassaoleva]
-      ].map(item => this.createDatasourceElement(item[0] as string, item[1] as number));
+        [this.i18n.yhteenveto_lapset_lkm, yhteenveto.lapset_lkm, this.i18n.yhteenveto_tooltip_lapset_lkm],
+        [this.i18n.yhteenveto_lapset_vakapaatos_voimassaoleva, yhteenveto.lapset_vakapaatos_voimassaoleva, this.i18n.yhteenveto_tooltip_lapset_vakapaatos_voimassaoleva],
+        [this.i18n.yhteenveto_lapset_vakasuhde_voimassaoleva, yhteenveto.lapset_vakasuhde_voimassaoleva, this.i18n.yhteenveto_tooltip_lapset_vakasuhde_voimassaoleva],
+        [this.i18n.yhteenveto_lapset_vuorohoidossa, yhteenveto.lapset_vuorohoidossa, this.i18n.yhteenveto_tooltip_lapset_vuorohoidossa],
+        [this.i18n.yhteenveto_lapset_palveluseteli_ja_ostopalvelu, yhteenveto.lapset_palveluseteli_ja_ostopalvelu, this.i18n.yhteenveto_tooltip_lapset_palveluseteli_ja_ostopalvelu],
+        [this.i18n.yhteenveto_lapset_maksutieto_voimassaoleva, yhteenveto.lapset_maksutieto_voimassaoleva, this.i18n.yhteenveto_tooltip_lapset_maksutieto_voimassaoleva]
+      ].map(item => this.createDatasourceElement(item[0] as string, item[1] as number, item[2] as string));
 
       this.datasourceToimipaikat = [
-        [this.i18n.yhteenveto_toimipaikat_voimassaolevat, yhteenveto.toimipaikat_voimassaolevat],
-        [this.i18n.yhteenveto_toimipaikat_paattyneet, yhteenveto.toimipaikat_paattyneet],
-        [this.i18n.yhteenveto_toimintapainotukset_maara, yhteenveto.toimintapainotukset_maara],
-        [this.i18n.yhteenveto_kielipainotukset_maara, yhteenveto.kielipainotukset_maara]
-      ].map(item => this.createDatasourceElement(item[0] as string, item[1] as number));
+        [this.i18n.yhteenveto_toimipaikat_voimassaolevat, yhteenveto.toimipaikat_voimassaolevat, this.i18n.yhteenveto_tooltip_toimipaikat_voimassaolevat],
+        [this.i18n.yhteenveto_toimipaikat_paattyneet, yhteenveto.toimipaikat_paattyneet, this.i18n.yhteenveto_tooltip_toimipaikat_paattyneet],
+        [this.i18n.yhteenveto_toimintapainotukset_maara, yhteenveto.toimintapainotukset_maara, this.i18n.yhteenveto_tooltip_toimintapainotukset_maara],
+        [this.i18n.yhteenveto_kielipainotukset_maara, yhteenveto.kielipainotukset_maara, this.i18n.yhteenveto_tooltip_kielipainotukset_maara]
+      ].map(item => this.createDatasourceElement(item[0] as string, item[1] as number, item[2] as string));
 
       this.datasourceTyontekijat = [
-        [this.i18n.yhteenveto_tyontekijat_lkm, yhteenveto.tyontekijat_lkm],
-        [this.i18n.yhteenveto_palvelussuhteet_voimassaoleva, yhteenveto.palvelussuhteet_voimassaoleva],
-        [this.i18n.yhteenveto_palvelussuhteet_maaraaikaiset, yhteenveto.palvelussuhteet_maaraaikaiset],
-        [this.i18n.yhteenveto_varhaiskasvatusalan_tutkinnot, yhteenveto.varhaiskasvatusalan_tutkinnot],
-        [this.i18n.yhteenveto_tyoskentelypaikat_kelpoiset, yhteenveto.tyoskentelypaikat_kelpoiset],
-        [this.i18n.yhteenveto_taydennyskoulutukset_kuluva_vuosi, yhteenveto.taydennyskoulutukset_kuluva_vuosi],
-        [this.i18n.yhteenveto_tilapainen_henkilosto_maara_kuluva_vuosi, yhteenveto.tilapainen_henkilosto_maara_kuluva_vuosi],
-        [this.i18n.yhteenveto_tilapainen_henkilosto_tunnit_kuluva_vuosi, yhteenveto.tilapainen_henkilosto_tunnit_kuluva_vuosi],
-      ].map(item => this.createDatasourceElement(item[0] as string, item[1] as number));
+        [this.i18n.yhteenveto_tyontekijat_lkm, yhteenveto.tyontekijat_lkm, this.i18n.yhteenveto_tooltip_tyontekijat_lkm],
+        [this.i18n.yhteenveto_palvelussuhteet_voimassaoleva, yhteenveto.palvelussuhteet_voimassaoleva, this.i18n.yhteenveto_tooltip_palvelussuhteet_voimassaoleva],
+        [this.i18n.yhteenveto_palvelussuhteet_maaraaikaiset, yhteenveto.palvelussuhteet_maaraaikaiset, this.i18n.yhteenveto_tooltip_palvelussuhteet_maaraaikaiset],
+        [this.i18n.yhteenveto_varhaiskasvatusalan_tutkinnot, yhteenveto.varhaiskasvatusalan_tutkinnot, this.i18n.yhteenveto_tooltip_varhaiskasvatusalan_tutkinnot],
+        [this.i18n.yhteenveto_tyoskentelypaikat_kelpoiset, yhteenveto.tyoskentelypaikat_kelpoiset, this.i18n.yhteenveto_tooltip_tyoskentelypaikat_kelpoiset],
+        [this.i18n.yhteenveto_taydennyskoulutukset_kuluva_vuosi, yhteenveto.taydennyskoulutukset_kuluva_vuosi, this.i18n.yhteenveto_tooltip_taydennyskoulutukset_kuluva_vuosi],
+        [this.i18n.yhteenveto_tilapainen_henkilosto_maara_kuluva_vuosi, yhteenveto.tilapainen_henkilosto_maara_kuluva_vuosi, this.i18n.yhteenveto_tooltip_tilapainen_henkilosto_maara_kuluva_vuosi],
+        [this.i18n.yhteenveto_tilapainen_henkilosto_tunnit_kuluva_vuosi, yhteenveto.tilapainen_henkilosto_tunnit_kuluva_vuosi, this.i18n.yhteenveto_tooltip_tilapainen_henkilosto_tunnit_kuluva_vuosi],
+      ].map(item => this.createDatasourceElement(item[0] as string, item[1] as number, item[2] as string));
     });
   }
 
-  createDatasourceElement(label: string, value: number): YhteenvetoElement {
+  createDatasourceElement(label: string, value: number, tooltip: string): YhteenvetoElement {
     return {
       name: label,
       value: value,
+      tooltip: tooltip,
       status: '',
     };
   }
