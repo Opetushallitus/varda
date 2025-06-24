17bf35b56af87e3e822012817affe74283446ce2
Varda <varda@csc.fi>
2025-06-24 08:01:04 +0000

    Public branch commit

---
+++ applications/frontend/projects/virkailija-app/src/app/shared/components/varda-delete-button/varda-delete-button.component.ts
@@ -0,0 +1,10 @@
+import {Component } from '@angular/core';
+
+@Component({
+  selector: 'app-varda-delete-button',
+  templateUrl: './varda-delete-button.component.html',
+  styleUrls: ['./varda-delete-button.component.css']
+})
+export class VardaDeleteButtonComponent {
+  constructor() { }
+}
